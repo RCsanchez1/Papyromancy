3260f9ed93e3d772f6403779d64ca1b60bef111d
Ruiz Sanchez <ruizcayetano@hotmail.com>
2020-07-06 15:22:11 -0600

    adding callback fuction

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,31 @@ const apiKey = '4a22645048860763a044';
 $(document).ready(function () {
     $('select').formSelect();
 });
+
+// Text-Processing API Url    - RC
+const API_URL = '';
+
+// RapidAPI request headers   
+const REQUEST_HEADERS = {
+  'X-RapidAPI-Host': ''
+  , 'X-RapidAPI-Key': '4a22645048860763a044'
+  , 'Content-Type': ''
+};
+
+const analyzeComment = (Currency, callback) => {
+    // Creating an object to send to the server
+    const data = {
+            text: Currency
+            , language: 'english'
+    };
+    // Encoding data for application/x-www-form-urlencoded content type   -RC
+    const formattedData = Qs.stringify(data);
+    // POST request to server
+    axios.post(API_URL, formattedData, { headers: REQUEST_HEADERS })
+            .then(response => {
+               const data = response.data;
+              // Calling a callback function with data from the server
+              callback(data)
+            })
+            .catch(error => console.error(error))
+  };
