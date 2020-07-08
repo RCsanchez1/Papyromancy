bb2b4a260a17f05defbb6bb1c975cc158d2367d0
Ammon Jeffery <savingphace@gmail.com>
2020-07-07 20:05:23 -0600

    testing freecurrencyapi request

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,18 @@
 
 const apiKey = '4a22645048860763a044';
-const apiKey2 ='';
+const apiKey2 ='00b641aadb7d3614e166';
 
 let PPP;
 let pppConversion;
+
+
 $(document).ready(function () {
     $('select').formSelect();
 
-    
+    axios.get("https://free.currconv.com/api/v7/convert?apiKey=" + apiKey2 + "&q=USD_PHP&compact=y")
+        .then((response) => {
+            console.log(response);
+        })
 });
 
 
@@ -22,6 +27,8 @@ $("#convert").on("click", function(){
 
         pppConversion = response.data.ppp.pppConversionFactor
         console.log(pppConversion);
+
+
     }
 
         )
