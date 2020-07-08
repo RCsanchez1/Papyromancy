6d01a373eb3ec659882309841ebe61afbccaaa60
Trevor Otterson <totterson97@gmail.com>
2020-07-07 18:40:56 -0600

    formatted documents to make more readable and fixed our header and logo size

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 const apiKey = '4a22645048860763a044';
 
 $(document).ready(function () {
-    $('select').formSelect();
+  $('select').formSelect();
 });
 
 // Text-Processing API Url    - RC
@@ -16,19 +16,19 @@ const REQUEST_HEADERS = {
 };
 
 const analyzeComment = (Currency, callback) => {
-    // Creating an object to send to the server
-    const data = {
-            text: Currency
-            , language: 'english'
-    };
-    // Encoding data for application/x-www-form-urlencoded content type   -RC
-    const formattedData = Qs.stringify(data);
-    // POST request to server
-    axios.post(API_URL, formattedData, { headers: REQUEST_HEADERS })
-            .then(response => {
-               const data = response.data;
-              // Calling a callback function with data from the server
-              callback(data)
-            })
-            .catch(error => console.error(error))
+  // Creating an object to send to the server
+  const data = {
+    text: Currency
+    , language: 'english'
   };
+  // Encoding data for application/x-www-form-urlencoded content type   -RC
+  const formattedData = Qs.stringify(data);
+  // POST request to server
+  axios.post(API_URL, formattedData, { headers: REQUEST_HEADERS })
+    .then(response => {
+      const data = response.data;
+      // Calling a callback function with data from the server
+      callback(data)
+    })
+    .catch(error => console.error(error))
+};
