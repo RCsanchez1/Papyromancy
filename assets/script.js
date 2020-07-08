e63f392a203f53c9fe3dfbf86a167f9d0f4dc2b0
talmage <talmagelucas@gmail.com>
2020-07-07 19:37:08 -0600

    fixed javascript to work for image select drop down, added button

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,17 @@
 const axios = ('axios').default;
 const apiKey = '4a22645048860763a044';
 
-$(document).ready(function(){
-    axios.get("https://api.purchasing-power-parity.com/?target=CA")
-    .then((response) => {
-        console.log(response);
-    }
 
-)});
+// DO NOT DELETE THIS
+$(document).ready(function () {
+    $('select').formSelect();
+});
+
+$(document).ready(function () {
+    axios.get("https://api.purchasing-power-parity.com/?target=CA")
+        .then((response) => {
+            console.log(response);
+        }
+
+        )
+});
