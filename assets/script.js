2e246643160e1b05e84798040a378710c1c756e1
Ammon Jeffery <savingphace@gmail.com>
2020-07-07 19:51:53 -0600

    testing

--- assets/script.js
+++ assets/script.js
@@ -2,20 +2,27 @@
 const apiKey = '4a22645048860763a044';
 const apiKey2 ='';
 
+let PPP;
+let pppConversion;
 $(document).ready(function () {
     $('select').formSelect();
+
+    
 });
 
 
-$(document).ready(function(){
+
+$("#convert").on("click", function(){
     axios.get("https://api.purchasing-power-parity.com/?target=CA&appid=" + apiKey)
     .then((response) => {
         console.log(response);
+
+        PPP = response.data.ppp.ppp;
+        console.log(PPP);
+
+        pppConversion = response.data.ppp.pppConversionFactor
+        console.log(pppConversion);
     }
 
         )
-});
-
-$("#convert").on("click", function(){
-
 })
