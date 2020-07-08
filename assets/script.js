7c58b921a7596ea38b595e8dd451fc8c3163a2cb
Ammon Jeffery <savingphace@gmail.com>
2020-07-07 19:28:41 -0600

    fixed axios request

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
-const axios = ('axios').default;
+
 const apiKey = '4a22645048860763a044';
 
 $(document).ready(function(){
-    axios.get("https://api.purchasing-power-parity.com/?target=CA")
+    axios.get("https://api.purchasing-power-parity.com/?target=CA&appid=" + apiKey)
     .then((response) => {
         console.log(response);
     }
