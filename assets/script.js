2b77e422bbe6487f0312d8b33b623b3c27229025
Ammon Jeffery <savingphace@gmail.com>
2020-07-07 19:24:06 -0600

    testing axios

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,10 @@
-//const axios = require('axios').default;
+const axios = ('axios').default;
 const apiKey = '4a22645048860763a044';
 
+$(document).ready(function(){
+    axios.get("https://api.purchasing-power-parity.com/?target=CA")
+    .then((response) => {
+        console.log(response);
+    }
 
+)});
